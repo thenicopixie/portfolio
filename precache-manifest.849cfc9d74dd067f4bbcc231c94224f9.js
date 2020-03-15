df69f0bdd7902a929df7f2d7dc50cb4a3b81ef0c
Nicole Swanson <nicswan93@gmail.com>
2020-03-15 14:29:54 -0500

    Updates

---
+++ precache-manifest.849cfc9d74dd067f4bbcc231c94224f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f527de08d67f908af6caa76ed6e3f5c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a3314ad655219d0ad3",
+    "url": "/static/css/main.741a2f87.chunk.css"
+  },
+  {
+    "revision": "cd8bc2d44ce52f7951db",
+    "url": "/static/js/2.a8d12baa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.a8d12baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a3314ad655219d0ad3",
+    "url": "/static/js/main.d753a7e5.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  },
+  {
+    "revision": "a4ade6c5219fff1c01145953e4668bac",
+    "url": "/static/media/backrow.a4ade6c5.png"
+  },
+  {
+    "revision": "072911f6e27848e6197da79bbacf24be",
+    "url": "/static/media/nicole.072911f6.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e381a3aa6ee490479d9fca0c895d7186.js"
+  "/precache-manifest.849cfc9d74dd067f4bbcc231c94224f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
